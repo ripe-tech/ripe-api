ee6163c79fc4ac6055176385854f741370cc22f3
joamag <joamag@hive.pt>
2017-01-10 16:10:05 +0000

    new setup file

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+
+setuptools.setup(
+    name = "ripe-api",
+    version = "0.1.0",
+    author = "Platforme International",
+    author_email = "development@platforme.com",
+    description = "RIPE API",
+    license = "Apache License, Version 2.0",
+    keywords = "ripe api",
+    url = "http://www.platforme.com",
+    zip_safe = False,
+    packages = [
+        "ripe"
+    ],
+    package_dir = {
+        "" : os.path.normpath("src")
+    },
+    install_requires = [
+        "appier"
+    ],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
+    ]
+)
