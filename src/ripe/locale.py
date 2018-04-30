c0670bf7af4f7af879f6b142fd67a1bc39fd3960
Hugo <hugo-gomes@users.noreply.github.com>
2018-04-30 11:51:19 +0100

    review format

--- src/ripe/locale.py
+++ src/ripe/locale.py
@@ -5,7 +5,8 @@ class LocaleAPI(object):
 
     def locale_m(self, values, locale = "en-us"):
         url = self.base_url + "locale"
-        contents = self.post(url,
+        contents = self.post(
+            url,
             values = values,
             locale = locale
         )
