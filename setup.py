881fa4dd8fe54af255f7115bc6f7d17a3c9d7cac
joamag <joamag@hive.pt>
2020-05-13 09:26:07 +0100

    Version 0.1.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name = "ripe-api",
-    version = "0.1.5",
+    version = "0.1.6",
     author = "Platforme International",
     author_email = "development@platforme.com",
     description = "RIPE API",
