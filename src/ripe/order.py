e1ae350c044600e32137b27b639a521f932710d7
Gabriel Candal <gcc@platforme.com>
2021-05-03 11:39:25 +0100

    feat: delete order

--- src/ripe/order.py
+++ src/ripe/order.py
@@ -27,6 +27,11 @@ class OrderAPI(object):
         contents = self.get(url)
         return contents
 
+    def delete_order(self, number):
+        url = self.base_url + "orders/%d" % number
+        contents = self.delete(url)
+        return contents
+
     def price_order(self, number, currency = None):
         url = self.base_url + "orders/%d/price" % number
         contents = self.get(url, currency = currency)
